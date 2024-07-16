7746803da386a4a6eefaf4958fb775d0a4f9e2ae
Simona Trotti <trottis@ynap.world>
2024-07-16 11:53:38 +0200

    setup start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+		<h1>Vite + React</h1>
+		<div className="card">
+			
+		</div>
+		<p className="read-the-docs">
+			Click on the Vite and React logos to learn more
+		</p>
+	</>
+  )
+}
+
+export default App
