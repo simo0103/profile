7208cdc67a72f31beb82cdb7d67465316e67b42a
Simona Trotti <trottis@ynap.world>
2024-09-04 13:43:29 +0200

    subcontent mobile

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,11 +5,15 @@ import React, {useState, useEffect} from 'react'
 
 const Header = () => {
 	const [prevScrollPos, setPrevScrollPos] = useState(0);
-	const [visible, setVisible] = useState(true)
+	const [isScrolled, setVisible] = useState(true)
+	const [isOnTop, setTop] = useState(false)
+
 
 	const handleScroll = () => {
 		const currentScrollPos = window.scrollY
 
+		currentScrollPos <= 50 ? setTop(true) : setTop(false);
+ 
 		if(currentScrollPos > prevScrollPos){
 			setVisible(false)
 		}else{
@@ -26,11 +30,11 @@ const Header = () => {
 
 	return (
 		<>
-			<div className="logo translate-x-[-50%] top-[3%] left-1/2 z-10 flex lg:hidden fixed">
+			<div className={`"${isOnTop ? " h-[100px]" : " h-[50px] bg-black"} logo z-50 flex justify-center w-full  items-center lg:hidden fixed "`}>
 				<h1 className="opacity-0 h-0 w-0 ">Santa Maria</h1>
-				<img className="h-16 wide:h-20" src={StMariaLogo} />
+				<img className={`"${isOnTop ? " h-16 " : " h-8 "} wide:h-20"`} src={StMariaLogo} />
 			</div>
-			<header className={`"${visible ? "mobile:translate-bezier" : " mobile:invisible mobile:translate-y-full mobile:translate-bezier "} mobile:bottom-[-60px] mobile:pb-[60px] mp-safe fixed lg:pb-inset left-0 right-0 lg:top-0 lg:bottom-auto width-full z-50 "`}>
+			<header className={`"${isScrolled ? "mobile:translate-bezier" : " mobile:invisible mobile:translate-y-full mobile:translate-bezier "} mobile:bottom-[-60px] mobile:pb-[60px] mp-safe fixed lg:pb-inset left-0 right-0 lg:top-0 lg:bottom-auto width-full z-50 "`}>
 				<Nav />
 			</header>
 		</>
--- src/components/home/SubContent.jsx
+++ src/components/home/SubContent.jsx
@@ -16,7 +16,7 @@ function SubContent() {
 	return (
 		<>
 			{!loaded && 
-				<div role="status" className="skeleton animate-pulse p-20 wide:px-80 flex justify-center items-center gap-8">
+				<div role="status" className="skeleton animate-pulse py-20 px-2 xl:px-80 xl:flex justify-center items-center gap-8">
 					<div className="flex-1">
 						<div className="h-4 bg-gray-600 rounded-full dark:bg-gray-800 mb-4"></div>
 						<div className="h-4 bg-gray-600 rounded-full dark:bg-gray-800 mb-4"></div>
@@ -26,15 +26,15 @@ function SubContent() {
 					<div className="flex-1 aspect-[4/3] bg-gray-600 rounded dark:bg-gray-800"></div>
 				</div>
 			}
-			<section id="subcontent" className="content p-20 wide:px-80 flex items-center gap-8">
-				<p className="text-white font-serif text-center text-lg leading-10 px-8 wide:text-xl">
+			<section id="subcontent" className="content py-20 px-2 xl:px-20 wide:px-80 xl:flex items-center gap-8">
+				<p className="text-white font-serif text-center mb-8 xl:mb-0 text-m wide:leading-10 wide:px-8 xl:text-xl">
 			
 					The pizza served in Santa Maria is exactly the same as the pizza you can
 					eat on the streets of Naples using the same ingredients and adopting the
 					same cooking method
 					
 				</p>
-				<img ref={image} src={trancioPizza} width="50%" style={{ display: loaded ? 'block' : 'none' }} />
+				<img ref={image} src={trancioPizza} className="w-full xl:w-2/4" width="50%" style={{ display: loaded ? 'block' : 'none' }} />
 			</section>
 		
 		</>
