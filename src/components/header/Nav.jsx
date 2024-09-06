467d47b83fc98f71d9f72a2c163f75f9192bc3d1
Simona Trotti <trottis@ynap.world>
2024-09-06 10:23:26 +0200

    icons

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -3,6 +3,8 @@ import { PiBasketLight } from "react-icons/pi";
 import StMariaLogo from "/smariawhite.svg";
 import { NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { BiFoodMenu } from "react-icons/bi";
+import { RiFunctionLine, RiReservedLine } from "react-icons/ri";
 
 const Nav = () => {
 	const [scrolling, setScrolling] = useState(false);
@@ -28,26 +30,28 @@ const Nav = () => {
 				<h1 className="opacity-0 h-0 w-0 ">Santa Maria</h1>
 				<img className="h-16 wide:h-20" src={StMariaLogo} />
 			</div>
-			<ul className="flex flex-1 justify-center items-center gap-8 font-sans text-m">
-				<li>
+			<ul className="flex flex-1 justify-center items-center center mobile:justify-around xl:gap-8 font-sans text-m">
+				<li className="flex mobile:flex-col items-center">
+				<RiFunctionLine className="text-white lg:hidden text-[30px]" />
 					<NavLink
 						to="/"
 						className={({ isActive }) =>
 							isActive
-								? "text-white wide:text-lg uppercase py-2 border-b-2 border-solid border-white"
-								: "wide:text-lg text-white uppercase py-2 border-b-2 border-solid border-transparent hover:border-white"
+								? "text-white wide:text-lg xl:uppercase xl:py-2 xl:border-b-2 xl:border-solid border-white"
+								: "wide:text-lg text-white xl:uppercase xl:py-2 xl:border-b-2 xl:border-solid border-transparent hover:border-white"
 						}
 					>
 						home
 					</NavLink>
 				</li>
-				<li>
+				<li className="flex mobile:flex-col items-center">
+					<RiFunctionLine className="text-white lg:hidden text-[30px]" />
 					<NavLink
 						to="/Menu"
 						className={({ isActive }) =>
 							isActive
-								? "text-white wide:text-lg uppercase py-2 border-b-2 border-solid border-white"
-								: "wide:text-lg text-white uppercase py-2 border-b-2 border-solid border-transparent hover:border-white"
+								? "text-white wide:text-lg xl:uppercase xl:py-2 xl:border-b-2 xl:border-solid border-white"
+								: "wide:text-lg text-white xl:uppercase xl:py-2 xl:border-b-2 xl:border-solid border-transparent hover:border-white"
 						}
 					>
 						menu
@@ -58,32 +62,34 @@ const Nav = () => {
 						to="/Ourstory"
 						className={({ isActive }) =>
 							isActive
-								? "text-white wide:text-lg uppercase py-2 border-b-2 border-solid border-white"
-								: "wide:text-lg text-white uppercase py-2 border-b-2 border-solid border-transparent hover:border-white"
+								? "text-white wide:text-lg xl:uppercase xl:py-2 xl:border-b-2 xl:border-solid border-white"
+								: "wide:text-lg text-white xl:uppercase xl:py-2 xl:border-b-2 xl:border-solid border-transparent hover:border-white"
 						}
 					>
 						our story
 					</NavLink>
 				</li>
-				<li>
+				<li className="flex mobile:flex-col items-center">
+					<RiFunctionLine className="text-white lg:hidden text-[30px]" />
 					<NavLink
 						to="/Orderonline"
 						className={({ isActive }) =>
 							isActive
-								? "text-white wide:text-lg uppercase py-2 border-b-2 border-solid border-white"
-								: "wide:text-lg text-white uppercase py-2 border-b-2 border-solid border-transparent hover:border-white"
+								? "text-white wide:text-lg xl:uppercase xl:py-2 xl:border-b-2 xl:border-solid border-white"
+								: "wide:text-lg text-white xl:uppercase xl:py-2 xl:border-b-2 xl:border-solid border-transparent hover:border-white"
 						}
 					>
 						order online
 					</NavLink>
 				</li>
-				<li>
+				<li className="flex mobile:flex-col items-center">
+					<RiFunctionLine className="text-white lg:hidden text-[30px]" />
 					<NavLink
 						to="/Booktable"
 						className={({ isActive }) =>
 							isActive
-								? "text-white wide:text-lg uppercase py-2 border-b-2 border-solid border-white"
-								: "wide:text-lg text-white uppercase py-2 border-b-2 border-solid border-transparent hover:border-white"
+								? "text-white wide:text-lg xl:uppercase xl:py-2 xl:border-b-2 xl:border-solid border-white"
+								: "wide:text-lg text-white xl:uppercase xl:py-2 xl:border-b-2 xl:border-solid border-transparent hover:border-white"
 						}
 					>
 						book a table
@@ -91,10 +97,10 @@ const Nav = () => {
 				</li>
 			</ul>
 			<ul className="hidden lg:flex font-sans text-m items-center gap-8">
-				<li>
+				<li className="flex align-center">
 					<PiBasketLight className="text-white text-lg wide:text-xl"></PiBasketLight>
 				</li>
-				<li className="text-white wide:text-lg">
+				<li className="text-white xl:text-lg">
 					<span>Sign Up</span>
 				</li>
 			</ul>
